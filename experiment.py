d5d4470fd63c41d4bfe8c17adff68fea16347573
Ngoc Huynh Bao <huynhbaongoc0412@gmail.com>
2020-09-18 15:16:08 +0200

    Distributed training

--- experiment.py
+++ experiment.py
@@ -28,22 +28,26 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    config = read_file(args.config_file)
-    (
-        Experiment(log_base_path=args.log_folder)
-        .from_full_config(config)
-        .run_experiment(
-            train_history_log=True,
-            model_checkpoint_period=args.model_checkpoint_period,
-            prediction_checkpoint_period=args.prediction_checkpoint_period,
-            epochs=args.epochs,
-        ).run_experiment(
-            train_history_log=True,
-            model_checkpoint_period=1,
-            prediction_checkpoint_period=1,
-            epochs=args.epochs + 1,
-            initial_epoch=args.epochs
+    strategy = tf.distribute.MirroredStrategy()
+    print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+
+    with strategy.scope():
+        config = read_file(args.config_file)
+        (
+            Experiment(log_base_path=args.log_folder)
+            .from_full_config(config)
+            .run_experiment(
+                train_history_log=True,
+                model_checkpoint_period=args.model_checkpoint_period,
+                prediction_checkpoint_period=args.prediction_checkpoint_period,
+                epochs=args.epochs,
+            ).run_experiment(
+                train_history_log=True,
+                model_checkpoint_period=1,
+                prediction_checkpoint_period=1,
+                epochs=args.epochs + 1,
+                initial_epoch=args.epochs
+            )
+            .plot_performance()
+            .plot_prediction(masked_images=[i for i in range(42)])
         )
-        .plot_performance()
-        .plot_prediction(masked_images=[i for i in range(42)])
-    )
